0453c6e275ad6a37c8f592a29057dc77cc9280ca
3durfey <3durfey@gmail.com>
2024-10-23 19:41:12 -0400

    changed import

--- prompt.py
+++ prompt.py
@@ -1,6 +1,21 @@
+"""
+
 def report_count(token):
     count = 0
     with open('/content/corpus.txt', 'r') as file:
         paragraph = file.read()
         count = tokenCount(token, paragraph)
     print(f"The term '{token}' shows up in the corpus {count} times.")
+"""
+def report_count(token):
+    count = 0
+    with open('/content/corpus.txt', 'r') as file:
+        paragraph = file.read()
+
+        inputArray = paragraph.split()
+        count = 0
+        for i in inputArray:
+          if i == token:
+            count += 1
+    print(f"The term '{token}' shows up in the corpus {count} times.")
+
