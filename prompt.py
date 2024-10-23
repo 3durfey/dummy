cdef44b6ef9fdd1ee41263059d9eb9a83ab7dc9a
3durfey <3durfey@gmail.com>
2024-10-23 19:25:00 -0400

    changed path

--- prompt.py
+++ prompt.py
@@ -1,6 +1,6 @@
 def report_count(token):
     count = 0
-    with open(file_path, 'r') as file:
+    with open('/content/corpus.txt', 'r') as file:
         paragraph = file.read()
         count = tokenCount(token, paragraph)
     print(f"The term '{token}' shows up in the corpus {count} times.")
