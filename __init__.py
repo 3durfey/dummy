5afbdc557d07b4ec335e6b3838dcaba8420523c5
3durfey <3durfey@gmail.com>
2024-10-23 21:05:08 -0400

    changes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,2 @@
-import sys
-sys.path.append('/content/dummy')
-
-
 from .helperFunctions import tokenCount
 file_path = '/content/corpus.txt'
--- prompt.py
+++ prompt.py
@@ -1,21 +1,7 @@
-"""
-
 def report_count(token):
-    count = 0
-    with open('/content/corpus.txt', 'r') as file:
-        paragraph = file.read()
-        count = tokenCount(token, paragraph)
-    print(f"The term '{token}' shows up in the corpus {count} times.")
-"""
-def report_count(token):
+    from .helperFunctions import tokenCount
     count = 0
     with open('dummy/corpus.txt', 'r') as file:
         paragraph = file.read()
-
-        inputArray = paragraph.split()
-        count = 0
-        for i in inputArray:
-          if i == token:
-            count += 1
+        count = tokenCount(token, paragraph)
     print(f"The term '{token}' shows up in the corpus {count} times.")
-
