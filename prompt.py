8dd5406ab0d51c161eaa78a2164ba6e6d2d9f49d
3durfey <3durfey@gmail.com>
2024-10-23 21:22:10 -0400

    s

--- prompt.py
+++ prompt.py
@@ -2,7 +2,7 @@ def report_count(token):
     from .helperFunctions import tokenCount
     import dummy
     count = 0
-    with open(dummy.p, 'r') as file:
+    with open('dummy/corpus', 'r') as file:
         paragraph = file.read()
         count = tokenCount(token, paragraph)
     print(f"The term '{token}' shows up in the corpus {count} times.")
